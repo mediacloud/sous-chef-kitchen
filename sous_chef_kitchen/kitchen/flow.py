1dc67ccebc572670afc83fa15282d138dedae903
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2025-06-09 15:59:00 -0400

    first pass at adding artifacts for persisting output

--- sous_chef_kitchen/kitchen/flow.py
+++ sous_chef_kitchen/kitchen/flow.py
@@ -7,7 +7,9 @@ from typing import Dict, List
 
 import prefect
 from prefect import flow, get_run_logger
+from prefect.artifacts import create_table_artifact
 from prefect.client.schemas.objects import FlowRun
+from prefect.context import FlowRunContext
 
 from sous_chef import RunPipeline, SousChefRecipe
 from sous_chef_kitchen.shared.recipe import get_recipe_folder
@@ -30,8 +32,16 @@ async def kitchen_base(recipe_name: str, tags: List[str] = [], parameters:Dict =
     with prefect.tags(*tags):
         run_data = RunPipeline(parsed_parameters)
 
-    # TODO: add task to cleanup return value (extract full_text)
+    # TODO: add task to cleanup return value (remove full_text [pending authentication test])
     # TODO: add task to create_table_artifact from rundata after cleanup
     #Just printing the run data now to validate. 
-    logger.info(run_data)
+    flow_run_name = FlowRunContext.get().flow_run.dict().get('name')
+    for task, output in run_data.items():
+        print(task)
+        print(output)
+        create_table_artifact(
+            key = flow_run_name+"::"+task,
+            table = output
+        )
+    
     return run_data
