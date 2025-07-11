cdd0784617accf02d97d07b6ce1e9462b4b8cafe
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2025-07-11 17:04:01 -0400

    sainity check logging

--- docker/prefect-config.py
+++ docker/prefect-config.py
@@ -77,10 +77,10 @@ def setup_secrets(overwrite=True):
     print("✅ Prefect Sous-Chef Secrets Setup")
 
 def run_prefect_deploy():
-    print("⏳ Waiting for Prefect Deploy to finish")
+    print("🖼️ Waiting for Prefect Deploy")
     #Feels a little hairbrained, but it gets the done job. 
     #Prefect has a python deployment pattern, but I think some deeper renovation would be needed to use it. 
-    subprocess.run(["prefect", "--no-prompt", "deploy", "--file", "../prefect.yaml", "--name", "kitchen-base"], check=True)
+    subprocess.run(["prefect", "--no-prompt", "deploy", "--file", "gitprefect.yaml", "--name", "kitchen-base"], check=True)
     print("✅ Prefect Sous-Chef Deployed")
 
 
@@ -88,4 +88,5 @@ if __name__ == "__main__":
     wait_for_api()
     asyncio.run(ensure_work_pool())
     setup_secrets()
+    print("This is happening...!")
     run_prefect_deploy()
