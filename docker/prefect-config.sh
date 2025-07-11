237cb5935eca43b028667714b3e5258646c50d0f
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2025-07-11 17:26:01 -0400

    add prefect-config.sh, to run additional deploy step

---
+++ docker/prefect-config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Run configuration steps againsts a prefect instance. 
+
+
+echo "==> Running prefect-config.py"
+python /app/prefect-config.py
+
+echo "==> Running prefect deploy"
+prefect --no-prompt deploy --name kitchen-base
